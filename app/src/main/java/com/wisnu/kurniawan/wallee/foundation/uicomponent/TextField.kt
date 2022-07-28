bac2d6faab4e00e049afb1d3583418c7808bb5a0
wisnu kurniawan <wisnuko13@gmail.com>
2022-07-29 06:39:43 +0700

    Bump androidxComposeMaterial3 = "1.0.0-alpha15"

--- app/src/main/java/com/wisnu/kurniawan/wallee/foundation/uicomponent/TextField.kt
+++ app/src/main/java/com/wisnu/kurniawan/wallee/foundation/uicomponent/TextField.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -78,6 +79,7 @@ fun PgTextField(
 }
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun PgTextField(
     value: TextFieldValue,
     onValueChange: (TextFieldValue) -> Unit,
